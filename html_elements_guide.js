203072dc30cad67ee348602a24c35b356c6a6416
mohammad mostafa <93571425+mohammadmostafaprogramer@users.noreply.github.com>
2025-07-20 02:34:15 +0330

    Upload

---
+++ html_elements_guide.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", () => {
+    // const sidebar = document.getElementById('summary_list');
+    // const initialTop = 50; // how far from top it starts
+    // const maxOffset = 0; // stop when it reaches this
+
+    // if (!sidebar) return;
+
+    // sidebar.style.position = 'relative';
+    // sidebar.style.top = `${initialTop}px`;
+
+    // window.addEventListener('scroll', () => {
+    //     const scrollY = window.scrollY;
+    //     const newTop = Math.max(initialTop - scrollY, maxOffset);
+    //     sidebar.style.top = `${newTop}px`;
+    //     sidebar.style.position = "fixed";
+    // });
+});
+
